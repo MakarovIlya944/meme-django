cb78ac0439a757ea38ceff0608d439cd4fdcfde2
Ilya Makarov <MakarovIlya944@yandex.ru>
2019-11-12 00:12:41 +0700

    Remove temp, add  dockerfile, update .sh

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.7-stretch
+
+COPY meme app
+WORKDIR /app
+COPY req.txt .
+RUN pip install -r req.txt
+
+RUN ["python", "manage.py", "migrate"]
+
+EXPOSE 8000
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
---
+++ update.sh
@@ -0,0 +1,10 @@
+echo 'Start updating';
+cp db.sqlite3 ./dumps/db.sqlite3;
+
+echo 'Building';
+docker build --force-rm -t meme ./meme-django;
+
+echo 'Runing';
+docker run --rm -v db.sqlite3:/app/db.sqlite3 -p 8080:8000 meme;
+
+echo 'Updated';
