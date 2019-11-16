63d818b9d27ba41e18be9d73ba14e18f653239a6
Ilya Makarov <makarovilya944@yandex.ru>
2019-11-17 02:12:55 +0700

    Add allowed host

--- meme/meme/settings.py
+++ meme/meme/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5p5#c603hd*9#h$^5_g=mabkyshcy=j=ge)@2an#g-4)!ux8dt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['217.71.129.139']
 
 
 # Application definition
