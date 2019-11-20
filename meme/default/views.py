d92db058a3a6960d4d5114f68b8d74efcb2a0b16
Ilya Makarov <makarovilya944@yandex.ru>
2019-11-20 21:54:13 +0700

    Add templates to signin signup

--- meme/default/views.py
+++ meme/default/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
+from default import forms
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
 def index(request):
     return render(request, "default/index.html")
+
+def signup(request):
+    return render(request, "default/signup.html")
+
+def signin(request):
+  
+    uservalue=''
+    passwordvalue=''
+
+    form= forms.Loginform(request.POST or None)
+    if form.is_valid():
+        uservalue= form.cleaned_data.get("username")
+        passwordvalue= form.cleaned_data.get("password")
+
+        user= authenticate(username=uservalue, password=passwordvalue)
+        if user is not None:
+            login(request, user)
+            context= {'form': form,
+                      'error': 'The login has been successful'}
+            
+            return render(request, 'default/signin.html', context)
+        else:
+            context= {'form': form,
+                      'error': 'The username and password combination is incorrect'}
+            
+            return render(request, 'default/signin.html', context )
+
+    else:
+        context= {'form': form}
+        return render(request, 'default/signin.html', context)
