6a0df88145a1a034ca619148f5d6dfd92d228ec8
Ilya Makarov <makarovilya944@yandex.ru>
2019-12-15 12:46:52 +0700

    Init npm package

--- live-server/server.js
+++ live-server/server.js
@@ -1,5 +1,7 @@
 const server = require("http").createServer();
 const io = require("socket.io")(server);
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('../meme/meme/db.sqlite3');
 const users = [];
 
 io.on("connection", client => {
@@ -16,4 +18,6 @@ function event(data) {
   io.send();
 }
 
-function updateUsers() {}
+function updateUsers() {
+  
+}
