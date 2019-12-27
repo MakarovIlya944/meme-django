7a15649ef24da982368a4c355168a9e6f9f7bece
Ilya Makarov <makarovilya944@yandex.ru>
2019-12-28 00:14:34 +0700

    Add simple working version

--- live-server/server.js
+++ live-server/server.js
@@ -3,6 +3,7 @@ const server_socket_io = http.createServer();
 const io = require("socket.io")(server_socket_io);
 
 const users = [];
+let clients = [];
 const server_django = new http.Server(function (req, res) {
   let userdata = '';
   res.setHeader('Content-Type', 'application/json');
@@ -13,7 +14,7 @@ const server_django = new http.Server(function (req, res) {
     // var result = userdata.split('&').map((p,i,r)=> { let x = p.split("=")[0]; { x:p.split("=")[1] }});
     const data = userdata.split('&');
     var result = {};
-    for (let i=0;i<data.length;i++) {
+    for (let i = 0; i < data.length; i++) {
       kv = data[i].split('=');
       result[kv[0]] = kv[1];
     }
@@ -24,44 +25,53 @@ const server_django = new http.Server(function (req, res) {
 server_django.listen(3030);
 
 io.on("connection", client => {
-  client.on("event", event);
-  client.on("disconnect", disconnect);
+  clients.push(client);
   console.log("Client connected");
-  io.emit("update");
+  client.on("update", update)
+  client.on("disconnect", disconnect);
 });
 server_socket_io.listen(3000);
 
-function disconnect() { }
+function disconnect() {
+  console.log("Client disconnected");
+}
+
+function update(data) {
+  console.log("Update")
+  updateUsers(data)
+}
 
 function event(data) {
-  io.send(data);
+  for (let i = 0; i < clients.length; i++)
+    clients[i].emit("event", data);
 }
 
 function updateUsers(data) {
-  let user = users.find((e, i, a) => { e.username === data.username });
-  if (user) {
-    if (data.action === 'login') {
-      user['login_time'] = data.time;
-      user.active = true;
+  if (data) {
+    let user = users.find((e, i, a) => { e.username === data.username });
+    if (user) {
+      if (data.action === 'login') {
+        user['login_time'] = data.time;
+        user.active = true;
+      }
+      else if (data.action === 'logout') {
+        user['logout_time'] = data.time;
+        user.active = false;
+      }
     }
-    else if (data.action === 'logout') {
-      user['logout_time'] = data.time;
-      user.active = false;
+    else {
+      user = {
+        'username': data.username,
+        'active': data.action === 'logout',
+        'logout_time': '',
+        'login_time': ''
+      }
+      if (data.action === 'logout')
+        user['logout_time'] = data.time;
+      else if (data.action === 'login')
+        user['login_time'] = data.time;
+      users.push(user);
     }
   }
-  else {
-    user = {
-      'username': data.username,
-      'active': data.action === 'logout',
-      'logout_time': '',
-      'login_time': ''
-    }
-    if (data.action === 'logout')
-      user['logout_time'] = data.time;
-    else if (data.action === 'login')
-      user['login_time'] = data.time;
-    users.push(user);
-  }
-
   event(users);
 }
--- meme/alias/static/js/src/client.js
+++ meme/alias/static/js/src/client.js
@@ -1,37 +1,42 @@
-
 import io from "./socket.io.js";
-// const io = require("socket.io-client");
 const socket = io("http://localhost:3000");
 socket.on("connect", connect);
 socket.on("event", event);
-socket.on("disconnect", disconnect);
 
-function connect(
-  
-) {console.log('Hello');}
+function connect() {
+  console.log('Hello');
+}
 
 function event(data) {
   console.log('event');
   updateTable(data);
 }
 
-function disconnect() {}
+window.onload = initialize;
 
-// window.onload = initialize;
+function initialize() {
+  var button = document.getElementById("alias-refresh");
+  button.onclick = refresh;
+}
 
-// function initialize() {
-//   var button = document.getElementById("alias-btn");
-//   document.onload = updateTable();
-// }
+function refresh() {
+  socket.emit("update", {
+    'username': 'test',
+    'action': 'login',
+    'login_time': '12312312eda'
+  });
+}
 
 function updateTable(data) {
   let table = document.getElementById("client-table");
   deleteRows(table);
-  rows = table.rows();
+  let rows = table.rows;
   console.log(rows);
-  for(let i=0;i<data.length;i++)
-  {
+  for (let i = 0; i < data.length; i++) {
     table.insertRow();
+    rows[i].className = "table-dark";
+    rows[i].innerHTML = 
+    `<td>${i}</td><td>${data[i].username}</td>`;
     console.log(table);
   }
 }
