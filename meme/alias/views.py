1a8bedad245f0de9383c1756a9c7104fa77d7644
Ilya Makarov <makarovilya944@yandex.ru>
2020-01-05 12:41:40 +0700

    Update view

--- meme/alias/views.py
+++ meme/alias/views.py
@@ -1,6 +1,17 @@
-# from django.shortcuts import render
 from django.template.response import TemplateResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
 def index(request):
     return TemplateResponse(request, "alias/index.html")
+
+class AliasIndex(View):
+
+    def get(self, request, *args, **kwargs):
+        return TemplateResponse(request, "alias/index.html")
+
+    def post(self, request, *args, **kwargs):
+        t = 'sdas'
+        team = request.POST.get('team')
+        return TemplateResponse(request, "alias/index.html")
