1e876735a26ee126b99181c236ea9fc631b7a720
Ilya Makarov <makarovilya944@yandex.ru>
2020-02-09 19:25:54 +0700

    Fix add

--- meme/hookah/views.py
+++ meme/hookah/views.py
@@ -1,20 +1,45 @@
 from django.views import View
 from django.template.response import TemplateResponse
 from hookah.models import Recipe, Tabacco
+from hookah.management.commands.tabaccos import construct_tobacco
+from django.shortcuts import redirect
+
 
 # Create your views here.
 
 
 class HookahIndex(View):
 
+    tabaccos = ''
+    recepies = ''
+
     def get(self, request, *args, **kwargs):
-        a = Recipe.objects.all()
+        recepies = Recipe.objects.all()
         recepies = [{'tobaccos': [{'taste': str(t),
                                    'have': 'list-group-item-primary' if t.Have else 'list-group-item-danger'} for t in e.TabaccoList.all()],
                      'value': e.price()}
-                    for e in a]
-        recepies = sorted(recepies, key=lambda x: -x['value'])
-        b = Tabacco.objects.all().filter(Have=True)
-        tabaccos = [{'mark': t.Mark, 'taste': t.Taste, 'icon': t.Icon, 'mass':t.Mass if t.Mass != 0 else None}
-                    for t in b]
-        return TemplateResponse(request, "hookah/index.html", context={'tabaccos': tabaccos, 'recepies': recepies})
+                    for e in recepies]
+        HookahIndex.recepies = sorted(recepies, key=lambda x: -x['value'])
+        tabaccos = Tabacco.objects.all().filter(Have=True)
+        HookahIndex.tabaccos = [{'mark': t.Mark, 'taste': t.Taste, 'icon': t.Icon, 'mass': t.Mass if t.Mass != 0 else None}
+                                for t in tabaccos]
+        return TemplateResponse(request, "hookah/index.html", context={'tabaccos': HookahIndex.tabaccos, 'recepies': HookahIndex.recepies})
+
+    def post(self, request, *args, **kwargs):
+
+        mass = request.POST.get('mass')
+        taste = request.POST.get('taste')
+        mark = request.POST.get('mark')
+        # if mass == '' or taste == '' or mark == '':
+        #     return TemplateResponse(request, "hookah/index.html",
+        #                             context={'tabaccos': HookahIndex.tabaccos,
+        #                                      'recepies': HookahIndex.recepies,
+        #                                      'error': {'mark': mark,
+        #                                                'taste': taste,
+        #                                                'mass': mass}})
+        t = construct_tobacco({'mark': mark, 'taste': taste})
+        t.Mass = mass
+        t.Have = True
+        t.save()
+
+        return redirect('/hookah')
